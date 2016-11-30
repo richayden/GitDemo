9980491e350392963b18b1ade80ee66261545918
Richard Hayden <richayden@Richards-MacBook-Pro.local>
2016-11-30 13:56:19 -0500

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Richard Hayden on 11/30/16.
+//  Copyright © 2016 Richard Hayden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
